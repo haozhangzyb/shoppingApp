ab974b21242539ade6d6ad7742c31f84981fabf6
Hao Zhang <haozhangzyb@outlook.com>
2023-01-24 10:56:11 +0800

    init backend nodejs express server

---
+++ src/server/routes/api/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   POST api/users
+// @desc    Register route
+// @access  Public
+router.post("/", async (req, res) => {
+  try {
+    // console.log(req.body);
+    // res.send("User route");
+    const { email, password } = req.body;
+    return res.json({ message: "Registration Success!" });
+  } catch (error) {
+    return res.status(500).send("Server error");
+  }
+});
---
+++ src/server/routes/index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  return res.json({ message: "Hello World1" });
+});
+
+module.exports = router;
