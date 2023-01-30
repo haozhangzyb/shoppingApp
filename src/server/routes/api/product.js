87332f0dc5d5f093f51748d610088c0027e37e4f
Hao Zhang <haozhangzyb@outlook.com>
2023-01-30 08:44:55 +0800

    add update product route and bug fix

--- src/server/routes/api/product.js
+++ src/server/routes/api/product.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-import Product from "../../models/product";
-import jwtTokenToUserId from "../../middleware/jwtTokenToUserId";
+import Product from "../../models/product.js";
+import jwtTokenToUserId from "../../middleware/jwtTokenToUserId.js";
 
 // @route   GET api/products
 // @desc    Get all products
@@ -12,6 +12,7 @@ router.get("/", async (req, res) => {
     const products = await Product.find();
     return res.json(products);
   } catch (error) {
+    console.error(error);
     return res.status(500).json("Server error");
   }
 });
@@ -24,6 +25,7 @@ router.get("/:id", async (req, res) => {
     const product = await Product.findById(req.params.id);
     return res.json(product);
   } catch (error) {
+    console.error(error);
     return res.status(500).json("Server error");
   }
 });
@@ -42,6 +44,7 @@ router.delete("/:id", jwtTokenToUserId, async (req, res) => {
     await product.remove();
     return res.json({ msg: "Product removed" });
   } catch (error) {
+    console.error(error);
     return res.status(500).json("Server error");
   }
 });
@@ -53,7 +56,7 @@ router.post("/", jwtTokenToUserId, async (req, res) => {
   try {
     const { name, description, category, price, quantity, image_url } =
       req.body;
-    const newProduct = new Product({
+    let newProduct = new Product({
       name,
       description,
       category,
@@ -63,14 +66,48 @@ router.post("/", jwtTokenToUserId, async (req, res) => {
       createdAt: Date.now(),
     });
 
-    let product = await Product.findOneAndUpdate(
-      { _id: req.body.id },
-      { $set: newProduct },
-      { new: true, upsert: true, setDefaultsOnInsert: true }
-    );
+    newProduct = await newProduct.save();
 
-    return res.json(product);
+    return res.json(newProduct);
   } catch (error) {
+    console.error(error);
     return res.status(500).json("Server error");
   }
 });
+
+// @route   PUT api/products/:id
+// @desc    Update product
+// @access  Private
+router.put("/:id", jwtTokenToUserId, async (req, res) => {
+  try {
+    const { name, description, category, price, quantity, image_url } =
+      req.body;
+
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ errors: [{ msg: "Product not found" }] });
+    }
+
+    product = new Product({
+      ...product,
+      name,
+      description,
+      category,
+      price,
+      quantity,
+      image_url,
+      updatedAt: Date.now(),
+    });
+
+    product = await product.save();
+
+    return res.json(product);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json("Server error");
+  }
+});
+
+export default router;
