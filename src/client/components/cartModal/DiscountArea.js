4e72e1013c1a4ad9f46e511373a3d4ffcbadc32c
Hao Zhang <haozhangzyb@outlook.com>
2023-02-09 10:21:38 -0600

    add dicount area validation

--- src/client/components/cartModal/DiscountArea.js
+++ src/client/components/cartModal/DiscountArea.js
@@ -1,7 +1,23 @@
-import { Box, Stack, TextField, Typography, Button } from "@mui/material";
 import React from "react";
+import { Box, Stack, TextField, Typography, Button } from "@mui/material";
+import * as yup from "yup";
+import { useFormik } from "formik";
 
 const DiscountArea = () => {
+  const formikFormData = useFormik({
+    initialValues: {
+      couponCode: "",
+    },
+    validationSchema: yup.object({
+      couponCode: yup
+        .string("Enter a valid coupon code")
+        .required("Enter a valid coupon code"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <Box sx={{ mt: 4 }}>
       <Typography
@@ -12,7 +28,23 @@ const DiscountArea = () => {
         Apply Discount Code
       </Typography>
       <Stack direction='row' spacing={2} sx={{ mt: 1 }}>
-        <TextField sx={{ flexGrow: 1 }} variant='outlined' size='small' />
+        <TextField
+          sx={{ flexGrow: 1 }}
+          variant='outlined'
+          size='small'
+          name='couponCode'
+          onChange={formikFormData.handleChange}
+          onBlur={formikFormData.handleBlur}
+          value={formikFormData.values.couponCode}
+          error={
+            formikFormData.touched.couponCode &&
+            Boolean(formikFormData.errors.couponCode)
+          }
+          helperText={
+            formikFormData.touched.couponCode &&
+            formikFormData.errors.couponCode
+          }
+        />
         <Button
           variant='contained'
           sx={{
@@ -21,6 +53,7 @@ const DiscountArea = () => {
             textTransform: "none",
             bgcolor: "#4f48dd",
           }}
+          onClick={formikFormData.handleSubmit}
         >
           Apply
         </Button>
